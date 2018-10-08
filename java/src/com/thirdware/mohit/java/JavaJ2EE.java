252ee28e4b6b6dfa030b3492f16cbf0e95c08ee4
MohitChhikara <43954213+MohitChhikara@users.noreply.github.com>
2018-10-08 17:11:29 +0530

    pushing Task 1
    First commit with logic ( For loops)

---
+++ java/src/com/thirdware/mohit/java/JavaJ2EE.java
@@ -0,0 +1,16 @@
+package com.thirdware.mohit.java;
+
+public class JavaJ2EE {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String s = "javaj2ee";
+     for(int i = 0; i < s.length(); i++)
+     {
+    	 for(int j = 0; j < i; j++)
+    	 {
+    		 System.out.print("hi");
+    	 }
+    	 System.out.println();
+     }
+	}
+}
