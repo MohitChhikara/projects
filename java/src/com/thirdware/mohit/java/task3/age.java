4dae0c5951782509fac16b57af1478b2eda1e885
MohitChhikara <43954213+MohitChhikara@users.noreply.github.com>
2018-10-09 14:52:03 +0530

    task4
    task completed

--- java/src/com/thirdware/mohit/java/task3/age.java
+++ java/src/com/thirdware/mohit/java/task3/age.java
@@ -1,10 +1,24 @@
 package com.thirdware.mohit.java.task3;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
 
 public class age {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	       System.out.print("Please enter date of birth in YYYY-MM-DD: ");
+	       Scanner sc = new Scanner(System.in);
+	       String dateOfBirth = sc.nextLine();
+	       
 
-	}
+	       LocalDate dob = LocalDate.parse(dateOfBirth);
+	       System.out.println("Age is:" + getAge(dob));
+	   }
 
-}
+	   public static int getAge(LocalDate dob) {
+	       LocalDate currentDate = LocalDate.now();
+	       return Period.between(dob, currentDate).getYears();
+	   }	
+	   }
+
+
